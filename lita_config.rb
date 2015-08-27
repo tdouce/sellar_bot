0152582c5bde889a14778d45ed87a1695b95e979
tdouce <travisdouce@gmail.com>
2015-08-27 11:19:58 -0400

    Create switch to for running lita in production and dev

--- lita_config.rb
+++ lita_config.rb
@@ -25,8 +25,13 @@ Lita.configure do |config|
   # config.adapter.password = "secret"
 
   ## Example: Set options for the Redis connection.
-  config.redis[:url] = ENV["REDISTOGO_URL"]
-  config.http.port = ENV.fetch("PORT")
+  if ENV["LITA_ENV"] == "production"
+    config.redis[:url] = ENV["REDISTOGO_URL"]
+    config.http.port = ENV.fetch("PORT")
+  else
+    config.redis.host = ENV.fetch('REDIS_HOST')
+    config.redis.port = ENV.fetch('REDIS_PORT')
+  end
 
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
