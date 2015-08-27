c01642e176cf37295b7de900ba1ee3026464d534
tdouce <travisdouce@gmail.com>
2015-08-27 10:46:10 -0400

    Sets RedisToGo url
    Use Redis To Go on heroku

--- lita_config.rb
+++ lita_config.rb
@@ -25,8 +25,7 @@ Lita.configure do |config|
   # config.adapter.password = "secret"
 
   ## Example: Set options for the Redis connection.
-  config.redis.host = ENV.fetch('REDIS_HOST')
-  config.redis.port = ENV.fetch('REDIS_PORT')
+  config.redis[:url] = ENV["REDISTOGO_URL"]
 
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
